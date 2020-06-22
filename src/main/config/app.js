b8b3cbe5f51ec545265fdd4bf5a2693370419ec3
Felipe Souza <felipedb91@gmail.com>
2020-06-21 21:57:56 -0300

    Add route file

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -1,8 +1,8 @@
-
+const setRoutes = require('./routes')
 const express = require('express')
 const app = express()
 const setupApp = require('./setup')
 
 setupApp(app)
-
+setRoutes(app)
 module.exports = app
