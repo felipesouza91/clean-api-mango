0c6ffca696acefcf03e7667d6d2d939d13c098bc
Felipe Souza <felipedb91@gmail.com>
2020-06-21 21:21:00 -0300

    Correct MongoHelp test connect with beforeAll method

--- src/infra/helpers/mongo-helper.spec.js
+++ src/infra/helpers/mongo-helper.spec.js
@@ -1,12 +1,16 @@
 const MongoHelp = require('./mongo-helper')
 
 describe('Mongo Helper', () => {
+  beforeAll(async () => {
+    await MongoHelp.connect(process.env.MONGO_URL)
+  })
+
   afterAll(async () => {
     await MongoHelp.disconnect()
   })
+
   test('Should reconnect when getDb is invoked and client is disconnected', async () => {
     const sut = MongoHelp
-    await sut.connect(process.env.MONGO_URL)
     expect(sut.db).toBeTruthy()
     await sut.disconnect()
     expect(sut.db).toBeFalsy()
